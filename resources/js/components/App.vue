38fa48d5a560f6cff9dcc40b5f2aa3f483037393
Roman <romanslex@gmail.com>
2019-01-09 21:01:31 +0300

    Включил SPA стандартными средствами

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Hello</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
