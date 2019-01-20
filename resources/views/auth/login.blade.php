472d794e23a985d54456e9f2c3007929d8ea3f1f
Roman <romanslex@gmail.com>
2019-01-20 20:18:28 +0300

    Сгенерировал стандартную авторизацию laravel Сделал страницу авторизацию

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,500,100italic,400,300italic&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
+    <title>Document</title>
+    <style>
+        *{margin: 0; padding: 0;}
+        body{
+            font-family: Roboto;
+            font-weight: 300;
+            font-size: 18px;
+            line-height: 1.5;
+        }
+        p{
+            margin: 5px 0;
+        }
+        .content{
+            display: grid;
+            grid-template-columns: 1fr 300px;
+            grid-gap: 50px;
+            width: 800px;
+            margin: 0 auto;
+            padding-top: 50px;
+        }
+
+        h1{
+            font-weight: 400;
+            font-size: 38px;
+        }
+        h3{
+            font-weight: 300;
+        }
+
+        .content .header{
+            margin-bottom: 1px;
+            margin-top: 10px;
+        }
+        .content input{
+            outline: none;
+            font-size: 18px;
+            padding: 5px;
+            width: 100%;
+        }
+        .content a{
+            text-decoration: none;
+            color: #275cad;
+            font-size: 13px;
+        }
+        .content button{
+            text-transform: uppercase;
+            width: 100%;
+            display: block;
+            padding: 8px 15px;
+            font-size: 14px;
+            font-weight: 400;
+            cursor: pointer;
+            transition: box-shadow 0.15s ease-out;
+            background: #5089e0;
+            box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.33);
+            color: white;
+            border: none;
+            margin-top: 10px;
+        }
+        .content button:hover{
+            background: #3274e0;
+            box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.38);
+        }
+
+        .content .error-block{
+            margin-top: 5px;
+        }
+        .content .error{
+            font-size: 13px;
+            color: red;
+        }
+
+        ul{
+            list-style: none;
+        }
+        ul li:before{
+            content: "—";
+            margin-right: 5px;
+        }
+    </style>
+</head>
+<body>
+<div class="content">
+    <div>
+        <h1>Привет!</h1>
+        <p>Это страница входа в тестовый сервис обратного звонка.</p>
+        <p>Данный сервис создан только в ознакомительных целях</p>
+        <p>Стек технологий:</p>
+        <ul>
+            <li>Laravel 5.7</li>
+            <li>Vue.js 2</li>
+            <li>PostgreSQL</li>
+            <li>pug, stylus etc</li>
+        </ul>
+    </div>
+    <div>
+        <h1>Вход в систему</h1>
+        <form class="login-form" method="post" action="/login">
+            {{csrf_field()}}
+
+            @if ($errors->any())
+                <div class="error-block">
+                    @foreach ($errors->all() as $error)
+                        <div class="error">{{ $error }}</div>
+                    @endforeach
+                </div>
+            @endif
+            <div class="header">Введите email</div>
+            <input type="text" value="admin">
+
+            <div class="header">Введите пароль</div>
+            <input type="password" style="margin-bottom: 5px;" value="admin">
+
+            <button type="submit">Войти</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", "Auth\LoginController@showLoginForm");
 
 Route::get("/home/orders/get-orders", "OrdersController@getOrders");
 
 Route::get('/home{any}', 'SpaController@index')->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
