c6993aa73dbe7d82133269fc3d4aa9fc964baa02
Roman <romanslex@gmail.com>
2019-01-22 10:53:51 +0300

    Защитил SpaController auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-Route::get("/", "Auth\LoginController@showLoginForm");
 
 Route::get("/home/orders/get-orders", "OrdersController@getOrders");
 
 Route::get('/home{any}', 'SpaController@index')->where('any', '.*');
 
 Auth::routes();
+Route::get("/", "Auth\LoginController@showLoginForm")->name("login");
 
 Route::get('/home', 'HomeController@index')->name('home');
