9d00f36a614af4d122ed8c731b393ad268e9f78d
Roman <romanslex@gmail.com>
2019-01-23 11:55:06 +0300

    Обратная связь

---
+++ resources/js/components/Pages/Feedback.vue
@@ -0,0 +1,83 @@
+<template lang="pug">
+    #feedback-content
+        h3 Напишите нам письмо
+        h4 Тема письма
+        div
+            select(name="title" v-model="title")
+                option(value="Вопрос по настройке виджета") Вопрос по настройке виджета
+                option(value="Сообщить о баге") Сообщить о баге
+                option(value="Оставить пожелание") Оставить пожелание
+                option(value="Другое") Другое
+        h4 Ваш вопрос
+            span(style="font-size: 11px")  (не более 1000 символов)
+        div: textarea(maxlength="1000" name="msg" id="msg" v-model="msg")
+        button.btn(@click="send") Отправить
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                title: "Вопрос по настройке виджета",
+                msg: "",
+                disable: false,
+            }
+        },
+        methods: {
+            send: function(){
+                if(this.disable) return;
+                this.disable = true;
+
+                window.axios
+                    .post("/data/feedback/send-comment", {title: this.title, msg: this.msg})
+                    .then(response => {
+                        this.$notifySuccess("Ваше сообщение отправлено");
+                        this.disable = false;
+                    })
+                    .catch(error => {
+                        this.$notifyDanger();
+                        this.disable = false;
+                    })
+            }
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+    #feedback-content
+        padding-top: 20px
+        width: 940px
+        margin: 0 auto
+    h4
+        margin-top: 10px
+        font-size: 15px
+        font-weight: 400
+        margin-bottom: 5px
+
+    select
+        font-size: 14px
+        padding: 5px
+
+    .btn
+        margin-top: 10px
+        font-size: 12px
+        padding: 8px 15px
+        width: max-content
+        border: none
+        color: white
+        background: #3497dc
+        cursor: pointer
+        text-transform: uppercase
+        outline: none
+        transition: box-shadow 0.15s ease-out
+        &:hover
+            box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.38)
+
+    #msg
+        font-size: 14px
+        font-family: Roboto
+        padding:5px
+        width: 100%
+        height: 150px
+        resize: none
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -6,6 +6,7 @@ import Orders from "./components/Pages/Orders.vue"
 import Widgets from "./components/Pages/Widgets/Widgets.vue"
 import Faq from "./components/Pages/Faq.vue"
 import Antispam from "./components/Pages/Antispam/Antispam.vue";
+import Feedback from "./components/Pages/Feedback.vue";
 
 const router = new VueRouter({
     mode: "history",
@@ -15,6 +16,7 @@ const router = new VueRouter({
         { path: '/home/widgets', component: Widgets },
         { path: '/home/faq', component: Faq },
         { path: '/home/antispam', component: Antispam },
+        { path: '/home/feedback', component: Feedback },
     ]
 });
 
