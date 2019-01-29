1792217e450b1c93e63c92c35748b0f92c5528c5
Roman <romanslex@gmail.com>
2019-01-29 18:52:36 +0300

    Убрал из сидера ptz-service

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\Payment::class, function (Faker $faker) {
     return [
         "info" => $faker->sentence(3),
-        "site" => "ptz-service.ru",
+        "site" => $faker->domainName,
         "sum" => $faker->numberBetween(0, 30000),
         "is_replenishment" => $faker->boolean,
         "performed_at" => $faker->dateTimeBetween("-4 month", "now")
